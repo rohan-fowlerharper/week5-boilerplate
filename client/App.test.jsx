74b92268de4162a6e6c415566070bbe201a954bf
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-06-30 21:32:07 +1200

    add test suite

---
+++ client/App.test.jsx
@@ -0,0 +1,20 @@
+/**
+ * @vitest-environment jsdom
+ */
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+
+describe('<App />', () => {
+  it('renders', () => {
+    render(<App />)
+    expect(screen.getByText(/dev/i)).toBeInTheDocument()
+  })
+  it('increments count correctly', async () => {
+    render(<App />)
+    expect(screen.getByText(/0/i)).toBeInTheDocument()
+    await userEvent.click(screen.getByRole('button'))
+    expect(screen.getByText(/1/i)).toBeInTheDocument()
+  })
+})
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: './test/setup.js',
+  },
   server: {
     proxy: {
       '/api': {
