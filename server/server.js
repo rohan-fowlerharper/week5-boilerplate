b0040271e5cdbfd868d3277586c79a7ea6af8980
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-07-27 22:58:55 +1200

    🔧fix client not being served in preview/production

--- server/server.js
+++ server/server.js
@@ -1,28 +1,29 @@
 const express = require('express')
-
+const path = require('path')
 const { createServer: createViteServer } = require('vite')
 
 const fruitsRouter = require('./routes/fruits.router')
 
-const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
 
+// use a function to create the server for async/await support
 async function createServer() {
   const server = express()
 
   let vite
-  if (!isProd) {
-    // 1. use the vite dev server in development mode
+  if (isDev) {
+    // use the vite dev server in development mode
     vite = await createViteServer({
       server: {
         middlewareMode: true,
       },
     })
   } else {
-    // 4. express.static (in production) needs to be before user-defined routes
+    // express.static (in preview/production) needs to be before user-defined routes
     server.use(express.static('dist'))
   }
 
-  // 2. user-defined routes and middleware
+  // user-defined routes and middleware
   server.use(express.urlencoded({ extended: true }))
   server.use('/api/fruits', fruitsRouter)
   server.get('/api/hello-world', (req, res) => {
@@ -33,9 +34,14 @@ async function createServer() {
     res.sendStatus(404)
   })
 
-  // 3. vite dev server middleware needs to be after user-defined routes
-  if (!isProd) {
+  // vite dev server middleware needs to be after user-defined routes
+  if (isDev) {
     server.use(vite.middlewares)
+  } else {
+    // serve client files in production after user-defined routes
+    server.use('*', (req, res) => {
+      res.sendFile(path.resolve('dist/index.html'))
+    })
   }
 
   return server
