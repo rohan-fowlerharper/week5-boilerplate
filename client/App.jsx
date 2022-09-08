f72ff9181c62394a68908b455c439607781c5abc
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-09-08 15:47:25 +1200

    feat: add link to second route

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
 
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 
-import * as api from '@/apiClient/fruits'
-
+import * as api from './apiClient/fruits'
 import logo from './logo.png'
 
 function App() {
@@ -19,6 +18,10 @@ function App() {
   return (
     <div className='App'>
       <div className='App-header'>
+        <Routes>
+          <Route path='/' element={<HomeRoute />} />
+          <Route path='hello' element={<SecondRoute />} />
+        </Routes>
         <img src={logo} alt='Dev Academy Logo' className='App-logo' />
         <h2>Hello DEV</h2>
         <div>
@@ -26,18 +29,35 @@ function App() {
           <button onClick={() => setCount((count) => count + 1)}>
             {count}
           </button>
-          <button onClick={fetchFruit}>Fetch Fruits</button>
+          <hr />
+          <div>
+            <button onClick={fetchFruit}>Fetch Fruits</button>
+          </div>
           {fruits.map((fruit) => (
             <div key={fruit.id}>
               {fruit.name}: {fruit.color}
             </div>
           ))}
-          <Routes>
-            <Route path='/' element={<div>Home Route</div>} />
-            <Route path='hello' element={<div>A second route at /hello</div>} />
-          </Routes>
         </div>
       </div>
+    </div>
+  )
+}
+
+function HomeRoute() {
+  return (
+    <div>
+      <h2>Home Route</h2>
+      <Link to='hello'>Go to a second route</Link>
+    </div>
+  )
+}
+
+function SecondRoute() {
+  return (
+    <div>
+      <h2>Second Route</h2>
+      <Link to='..'>Go back home</Link>
     </div>
   )
 }
